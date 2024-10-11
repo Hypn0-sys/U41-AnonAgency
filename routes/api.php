82646bc1efebb6bd8ea7473c497d7491b79c78fb
RLE <romain.leroy.fr@gmail.com>
2024-10-11 11:27:47 +0200

    c.f.l.a.g = CommentsAreEvils

--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+// Taking Routes
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
